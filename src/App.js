dcaa671832f02998bab58b233bea1b1322302f14
devika rajeevan <devikarajeevan2004@gmail.com>
2024-07-05 12:34:03 +0530

    nothing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './components/Navbar/Navbar.jsx';
+import Navbar from './Components/Navbar/Navbar.jsx';
 
 import BackgroundSlider from "./Components/Background/BackgroundSlider";
 
